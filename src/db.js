7ec2f4c9c02e9236e1cc6ea9ac9483b0479a489e
Arda Pektaş <ardaogulcan@gmail.com>
2017-03-24 23:23:59 +0200

    Update development environment (#8)

---
+++ src/db.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+import config from './config/environment';
+
+export default (callback) => {
+  const connectionString = `mongodb://${config.db.username}:${config.db.password}@${config.db.host}:${config.db.port}/${config.db.name}`;
+  const connectionOptions = {
+    server: {
+      socketOptions: {
+        keepAlive: config.db.keepAlive,
+        connectTimeOutMS: config.db.timeout,
+      },
+    },
+    replset: {
+      socketOptions: {
+        keepAlive: config.db.keepAlive,
+        connectTimeOutMS: config.db.timeout,
+      },
+    },
+  };
+
+  mongoose.connect(connectionString, connectionOptions);
+
+  callback(mongoose.connection);
+};
--- src/index.js
+++ src/index.js
@@ -1,26 +1,36 @@
 import http from 'http';
 import express from 'express';
 import compression from 'compression';
+import helmet from 'helmet';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import v1 from './v1';
-import config from './configs/environment';
+import config from './config/environment';
+import initializeDb from './db';
 
 const app = express();
 app.server = http.createServer(app);
 
 app.use(compression());
 app.use(cors(config.cors));
+app.use(helmet());
 
 app.use(bodyParser.json({
   limit: config.body.limit,
 }));
 
-app.use('/v1', v1(app));
-app.server.listen(process.env.PORT || config.env.port);
+app.use(bodyParser.urlencoded({
+  limit: config.body.limit,
+  extended: true,
+}));
 
-// eslint-disable-next-line
-console.log(`Started on port ${app.server.address().port}`);
+initializeDb(() => {
+  app.use('/v1', v1(app));
+  app.server.listen(process.env.PORT || config.env.port);
+
+  // eslint-disable-next-line
+  console.log(`Started on port ${app.server.address().port}`);
+});
 
 process.on('SIGINT', () => {
   app.server.close(() => {
