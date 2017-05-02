7b0bae032347e27f93445d269fb68d96da54affa
Arda Pektas <ardaogulcan@gmail.com>
2017-05-02 12:54:37 +0300

    Add user spesific end points

--- src/v1/resources/users/users.spec.js
+++ src/v1/resources/users/users.spec.js
@@ -4,9 +4,63 @@ import slugHero from 'mongoose-slug-hero';
 
 import { app } from '../../index.spec';
 import User from './user';
+import Board from '../boards/board';
 
 chai.should();
 chai.use(chaiHttp);
+
+function createTestQueryObject() {
+  return {
+    type: 'queryType',
+    term: 'term',
+  };
+}
+
+function createTestBoxObject() {
+  const testBox = {
+    title: 'Test Box Title',
+    description: 'Description about box',
+    refresh: { interval: 60000 },
+    queries: [],
+    position: {
+      top: 0,
+      left: 0,
+    },
+    size: {
+      width: 0,
+      height: 0,
+    },
+    style: {
+      color: '#000',
+      backgroundColor: '#FFF',
+    },
+  };
+
+  for (let i = 0; i < 5; i += 1) {
+    testBox.queries.push(createTestQueryObject());
+  }
+
+  return testBox;
+}
+
+function createTestBoardObject(creator) {
+  const testBoard = new Board();
+  testBoard.title = 'Test Title';
+  testBoard.creator = creator;
+  testBoard.boxes = [];
+
+  for (let i = 0; i < 5; i += 1) {
+    testBoard.boxes.push(createTestBoxObject());
+  }
+
+  return testBoard;
+}
+
+function createTestBoard(creator) {
+  return new Promise((resolve, reject) => {
+    createTestBoardObject(creator).save().then(resolve, reject);
+  });
+}
 
 function createTestUserObject() {
   const testUser = new User();
@@ -21,15 +75,23 @@ function createTestUser() {
     createTestUserObject().save().then(resolve, reject);
   });
 }
+function createTestUserWithBoard() {
+  return new Promise((resolve, reject) => {
+    createTestUserObject().save().then((user) => {
+      createTestBoard(user._id).then(board => resolve({user, board}), reject); // eslint-disable-line
+    }, reject);
+  });
+}
 
 describe('Resource: Users', () => {
   beforeEach((done) => {
     const userPromise = User.remove({});
+    const boardsPromise = Board.remove({});
     // eslint-disable-next-line
     const slugModel = slugHero.Counters['_slug_ctrs'];
     const slugPromise = slugModel.remove({});
 
-    Promise.all([userPromise, slugPromise]).then(() => done());
+    Promise.all([userPromise, boardsPromise, slugPromise]).then(() => done());
   });
 
   // List Users
@@ -216,6 +278,38 @@ describe('Resource: Users', () => {
         .end((err, res) => {
           res.should.have.status(200);
           res.body.should.be.a('object');
+          done();
+        });
+      });
+    });
+  });
+
+  // GET All User Boards
+  describe('/GET users/:userSlug/boards', () => {
+    it('it should get all boards of user with slug', (done) => {
+      createTestUserWithBoard()
+      .then(({ user }) => {
+        chai.request(app)
+        .get(`/v1/users/${user.slug}/boards`)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an('array');
+          done();
+        });
+      });
+    });
+  });
+
+  // GET User board with slug
+  describe('/GET users/:userSlug/boards/:boardSlug', () => {
+    it('it should get user board with slug', (done) => {
+      createTestUserWithBoard()
+      .then(({ user, board }) => {
+        chai.request(app)
+        .get(`/v1/users/${user.slug}/boards/${board.slug}`)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an('object');
           done();
         });
       });
